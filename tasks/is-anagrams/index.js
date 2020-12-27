b64d9be3f3bca443c8f069a30490843fa7aa6d77
Andrew Rusanov <andrew.rusanoff@gmail.com>
2020-12-27 20:00:53 +0300

    fix: isAnagrams function in one line

--- tasks/is-anagrams/index.js
+++ tasks/is-anagrams/index.js
@@ -1,7 +1,3 @@
-export const isAnagrams = (str1 = '', str2 = '') => {
-  if (!str1 || !str2) {
-    return false;
-  }
-
-  return str1.toLowerCase().split('').sort().join('') === str2.toLowerCase().split('').sort().join('');
-};
+export const isAnagrams = (str1 = '', str2 = '') => (!str1 || !str2)
+  ? false
+  : str1.toLowerCase().split('').sort().join('') === str2.toLowerCase().split('').sort().join('');
