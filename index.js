1aa501146b3526b79adc3d2f504fc96ed73ad101
Andrew Rusanov <andrew.rusanoff@gmail.com>
2020-12-27 20:53:59 +0300

    Export functions

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
-import { isPrime } from './tasks/is-prime';
-import { factorial } from './tasks/factorial';
-import { fib } from './tasks/fib';
-import { lodashGetAnalog } from './tasks/lodash-get-analog';
 import { validateBraces } from './tasks/braces-validator';
-import { isSorted } from './tasks/is-sorted';
+import { checkPairSum } from './tasks/check-pair-sum';
+import { checkWordInMatrix } from './tasks/check-word-in-matrix';
+import { compressSymbols } from './tasks/compress-symbols';
+import { cutToRange } from './tasks/cut-to-range';
+import { factorial } from './tasks/factorial';
+import { fib, fibRec } from './tasks/fib';
 import { filter } from './tasks/filter';
+import { findUnique } from './tasks/find-unique';
+import { fizzBuzz } from './tasks/fizz-buzz-100';
+import { flatten1, flatten2 } from './tasks/flat';
+import { formatArrayToText } from './tasks/format-array-to-text';
+import { isAnagrams } from './tasks/is-anagrams';
+import { isPalindrome } from './tasks/is-palindrome';
+import { isPrime } from './tasks/is-prime';
+import { isSorted } from './tasks/is-sorted';
+import { largestN } from './tasks/largest-n';
+import { lodashCarryAnalog } from './tasks/lodash-curry-analog';
+import { lodashGetAnalog } from './tasks/lodash-get-analog';
+import { longestPalindrome } from './tasks/longest-palindrome';
+import { longestSequence } from './tasks/longest-sequence';
+import { maxPurchases } from './tasks/max-purchases';
+import { missedNumber } from './tasks/missed-number';
+import { extendNumberPrototype } from './tasks/number-plus-method';
+import { stringCalc } from './tasks/string-calculator';
 
 export {
-  isPrime,
+  validateBraces,
+  checkPairSum,
+  checkWordInMatrix,
+  compressSymbols,
+  cutToRange,
   factorial,
   fib,
-  lodashGetAnalog,
-  validateBraces,
-  isSorted,
+  fibRec,
   filter,
+  findUnique,
+  fizzBuzz,
+  flatten1,
+  flatten2,
+  formatArrayToText,
+  isAnagrams,
+  isPalindrome,
+  isPrime,
+  isSorted,
+  largestN,
+  lodashCarryAnalog,
+  lodashGetAnalog,
+  longestPalindrome,
+  longestSequence,
+  maxPurchases,
+  missedNumber,
+  extendNumberPrototype,
+  stringCalc,
 };
