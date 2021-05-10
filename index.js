a0d637b3c0cf7112fb19f5b79dabdc40710598ac
Andrey Rusanov <andrew.rusanoff@gmil.com>
2021-05-10 05:20:31 +0300

    fix: Root exports

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { validateBraces } from './tasks/braces-validator';
 import { checkPairSum } from './tasks/check-pair-sum';
 import { checkWordInMatrix } from './tasks/check-word-in-matrix';
 import { compressSymbols } from './tasks/compress-symbols';
+import { query, where, order } from './tasks/currying-functions';
 import { cutToRange } from './tasks/cut-to-range';
 import { factorial } from './tasks/factorial';
 import { fib, fibRec } from './tasks/fib';
@@ -10,6 +11,7 @@ import { findUnique } from './tasks/find-unique';
 import { fizzBuzz } from './tasks/fizz-buzz-100';
 import { flatten1, flatten2 } from './tasks/flat';
 import { formatArrayToText } from './tasks/format-array-to-text';
+import { fuzzySearch } from './tasks/fuzzy-search';
 import { isAnagrams } from './tasks/is-anagrams';
 import { isPalindrome } from './tasks/is-palindrome';
 import { isPrime } from './tasks/is-prime';
@@ -29,16 +31,16 @@ export {
   checkPairSum,
   checkWordInMatrix,
   compressSymbols,
+  query, where, order,
   cutToRange,
   factorial,
-  fib,
-  fibRec,
+  fib, fibRec,
   filter,
   findUnique,
   fizzBuzz,
-  flatten1,
-  flatten2,
+  flatten1, flatten2,
   formatArrayToText,
+  fuzzySearch,
   isAnagrams,
   isPalindrome,
   isPrime,
